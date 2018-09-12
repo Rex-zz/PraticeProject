810c9fa4c9a5c42b9d249d0fb3b7375377036afd
Martvey <137418235@qq.com>
2018-09-12 19:12:21 +0800

    规范了接口

--- PraticeProject/src/com/pratice/dao/impl/StudentDaoImpl.java
+++ PraticeProject/src/com/pratice/dao/impl/StudentDaoImpl.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import com.pratice.dao.StudentDao;
 import com.pratice.entity.Student;
+/**
+ * 继承BaseDaoImpl方便获取SessionFactory的session
+ * 实现StudentDao实现基本的增删改查
+ * @author 137418235
+ *
+ */
 @Repository
 public class StudentDaoImpl extends BaseDaoImpl implements StudentDao {
 
